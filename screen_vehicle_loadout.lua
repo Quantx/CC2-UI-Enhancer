53f14c8a18c985531e21a62d8197ff57c39770e9
Samuel Deutsch <sam.e.deutsch@gmail.com>
2021-09-01 18:47:34 -0400

    New patch

--- screen_vehicle_loadout.lua
+++ screen_vehicle_loadout.lua
@@ -109,13 +109,19 @@ function update(screen_w, screen_h, ticks)
 
         update_add_ui_interaction_special(update_get_loc(e_loc.interaction_navigate), e_ui_interaction_special.gamepad_dpad_all)
 
+        local attached_vehicle = update_get_map_vehicle_by_id(this_vehicle:get_attached_vehicle_id(selected_bay_index))
+
         if selected_bay_index ~= -1 then
-            update_add_ui_interaction(update_get_loc(e_loc.interaction_select), e_game_input.interact_a)
+            if attached_vehicle:get() == false or attached_vehicle:get_dock_state() == e_vehicle_dock_state.docked then
+                update_add_ui_interaction(update_get_loc(e_loc.interaction_select), e_game_input.interact_a)
+            end
         end
 
         if is_pressed and selected_bay_index ~= -1 then
-            g_screen_index = 1
-            g_selected_bay_index = selected_bay_index
+            if attached_vehicle:get() == false or attached_vehicle:get_dock_state() == e_vehicle_dock_state.docked then
+                g_screen_index = 1
+                g_selected_bay_index = selected_bay_index
+            end
         end
 
         ui:end_window()
@@ -133,6 +139,11 @@ function update(screen_w, screen_h, ticks)
 
         local attached_vehicle = update_get_map_vehicle_by_id(this_vehicle:get_attached_vehicle_id(g_selected_bay_index))
         local is_show_attachment_selector = false
+
+        if attached_vehicle:get() and attached_vehicle:get_dock_state() ~= e_vehicle_dock_state.docked then
+            g_screen_index = 0
+            g_selected_bay_index = 0
+        end
 
         if g_screen_index == 1 then
             -- vehicle loadout
